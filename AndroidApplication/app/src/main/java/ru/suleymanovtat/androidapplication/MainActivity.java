972bfa15a3b689dd606adbf02951bb0860508367
suleymanovtat <suleymanovtat@gmail.com>
2017-06-02 18:10:48 +0300

    send data from to another fragment

--- AndroidApplication/app/src/main/java/ru/suleymanovtat/androidapplication/MainActivity.java
+++ AndroidApplication/app/src/main/java/ru/suleymanovtat/androidapplication/MainActivity.java
@@ -3,12 +3,14 @@ package ru.suleymanovtat.androidapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ru.suleymanovtat.androidapplication.framgent.TransactionFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new TransactionFragment(), TransactionFragment.TAB_TRANSACTION_FRAG).commit();
     }
 }
